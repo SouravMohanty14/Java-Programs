ffc5d162a69eda1aba413fd27027909292255714
Sourav Mohanty <sourav.mohanty93@gmail.com>
2017-01-10 12:28:44 +0530

    Update LoginCredential.java

--- MVCDemo/LoginCredential.java
+++ MVCDemo/LoginCredential.java
@@ -1,29 +1,30 @@
 package model;
-public class LogicCredential
+
+public class LoginCredential
 {
- private string uname, passwd;
- //jdbc code
- public boolean CheckLogin()
-  {
-  if(uname.equals("Sourav") && passwd.equals("123456"))
-  return true;
-  else
-  return false;
-  }
- void setuname(String uname)
- {
- this.uname = uname;
- }
- void setpasswd(String passwd)
- {
- this.passwd = passwd;
- }
- String getuname()
- {
- return uname;
- }
- String getpasswd()
- {
- return passwd;
- }
-}
+	private String uname, passwd;
+	//jdbc code
+	public boolean checkLogin()
+	{
+		if(uname.equals("Sourav") && passwd.equals("123456"))
+			return true;
+		else
+			return false;
+		}
+	public void setUname(String uname)
+	{
+		this.uname = uname;
+		}
+	public void setPasswd(String passwd)
+	{
+		this.passwd = passwd;
+		}
+	public String getUname()
+	{
+		return uname;
+		}
+	public String getPasswd()
+	{
+		return passwd;
+		}
+	}
